ba63a7306dece3a8f56c5d26ea1ac357cfc14b7f
Michael Onyekwere <onyekweremichael55@gmail.com>
2024-11-23 15:30:40 -0800

    updated

--- src/app/component/ShoppingCartModal.tsx
+++ src/app/component/ShoppingCartModal.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { MouseEvent } from 'react';
+import React, { MouseEvent, useEffect } from 'react';
 import { AiOutlineShopping } from 'react-icons/ai';
 import {
   Sheet,
